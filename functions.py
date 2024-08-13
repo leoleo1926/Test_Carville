cc56b0e4b52646756697a8f2e12f4c4507590ecc
leoleo1926 <147492773+leoleo1926@users.noreply.github.com>
2024-08-13 15:01:23 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,14 +3,14 @@ def calc_prices(InputPriceWithNDS, ProcNDS):
     if (0 <= ProcNDS <= 99):
         
         CorrectedPriceWithoutNDS = round(InputPriceWithNDS / (1 + ProcNDS / 100), 2)
-     # Убираем дробную часть, если надо
+     
         CorrectedPriceWithoutNDS = int( CorrectedPriceWithoutNDS * 100) / 100
-    # Расчет цены с НДС
-    # Приводим к нужному виду, округляем до 2 знаков после запятой без округления
+     # Р Р°СЃС‡РµС‚ С†РµРЅС‹ СЃ РќР”РЎ
+     # РџСЂРёРІРѕРґРёРј Рє РЅСѓР¶РЅРѕРјСѓ РІРёРґСѓ, РѕРєСЂСѓРіР»СЏРµРј РґРѕ 2 Р·РЅР°РєРѕРІ РїРѕСЃР»Рµ Р·Р°РїСЏС‚РѕР№ Р±РµР· РѕРєСЂСѓРіР»РµРЅРёСЏ
         CorrectedPriceWithNDS = round(CorrectedPriceWithoutNDS * (1 + ProcNDS / 100), 2)
-    # Проверка на соответствие условию по ценам (максимум 2 знака после запятой)
-        CorrectedPriceWithoutNDS= int(CorrectedPriceWithoutNDS * 100) / 100
     
+        CorrectedPriceWithoutNDS= int(CorrectedPriceWithoutNDS * 100) / 100
+     # РџСЂРѕРІРµСЂРєР° РЅР° СЃРѕРѕС‚РІРµС‚СЃС‚РІРёРµ СѓСЃР»РѕРІРёСЋ РїРѕ С†РµРЅР°Рј (РјР°РєСЃРёРјСѓРј 2 Р·РЅР°РєР° РїРѕСЃР»Рµ Р·Р°РїСЏС‚РѕР№)
         return CorrectedPriceWithNDS, CorrectedPriceWithoutNDS
     else: 
         return "NDS value error. The value must be in the range from 0 to 99"
