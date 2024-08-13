cc659c0a010a13b6a8f935a6195158e11b2ffcda
leoleo1926 <147492773+leoleo1926@users.noreply.github.com>
2024-08-13 15:00:26 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,18 +3,18 @@ from functions import calc_prices
 
 class TestCalcPrices(unittest.TestCase):
     def test_valid_input(self):
-        # Проверяем корректные входные данные
+       # РџСЂРѕРІРµСЂСЏРµРј РєРѕСЂСЂРµРєС‚РЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
         self.assertEqual(calc_prices(1.81, 20), (1.81, 1.51))
         self.assertEqual(calc_prices(1.81, 18), (1.81, 1.53))
         self.assertEqual(calc_prices(5.05, 27), (5.05, 3.98))
 
     def test_invalid_nds(self):
-        # Проверяем обработку некорректного значения НДС
+        # РџСЂРѕРІРµСЂСЏРµРј РѕР±СЂР°Р±РѕС‚РєСѓ РЅРµРєРѕСЂСЂРµРєС‚РЅРѕРіРѕ Р·РЅР°С‡РµРЅРёСЏ РќР”РЎ
         self.assertEqual(calc_prices(1.81, -5), "NDS value error. The value must be in the range from 0 to 99")
         self.assertEqual(calc_prices(1.81, 100), "NDS value error. The value must be in the range from 0 to 99")
 
     def test_rounding(self):
-        # Проверяем, что цены содержат максимум 2 знака после запятой
+       # РџСЂРѕРІРµСЂСЏРµРј, С‡С‚Рѕ С†РµРЅС‹ СЃРѕРґРµСЂР¶Р°С‚ РјР°РєСЃРёРјСѓРј 2 Р·РЅР°РєР° РїРѕСЃР»Рµ Р·Р°РїСЏС‚РѕР№
         self.assertEqual(calc_prices(5.055, 27), (5.05, 3.98))
         self.assertEqual(calc_prices(1.815, 18), (1.82, 1.54))
 
